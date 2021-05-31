1d42f6e75818b18e03c4fb5facf0629c518a5b08
Aver_Niko <froloff92@mail.ru>
2021-05-31 15:38:56 +0300

    first commit

--- ml_project/src/enities/feature_params.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-
-@dataclass()
-class FeatureParams:
-    numerical_features: List[str]
-    categorical_features: List[str]
-    target_col: str
--- ml_project/src/enities/gridsearch_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class GridSearchParams:
-    param_grid: dict
-    cv: int = field(default=5)
-    scoring: str = field(default='roc_auc')
